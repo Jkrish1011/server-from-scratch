7f2bc5e65bd8532e0d2b1206219c6ec673aee383
Jayakrishnan <jayakrishnanashok@gmail.com>
2026-02-08 01:16:57 +0400

    get_lines_channel done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use tokio::{
     fs::File,
     io::AsyncReadExt,
+    sync::mpsc,
 };
+use std::error::Error;
 
-#[tokio::main]
-async fn main() -> tokio::io::Result<()>{
-    let mut file = File::open("messages.txt").await?;
+
+fn print_type<T>(_: &T) { 
+    println!("{:?}", std::any::type_name::<T>());
+}
+
+async fn get_lines_channel(mut file: File, tx: mpsc::Sender<String>) -> Result<(), Box<dyn Error>> {
     let mut chunk = vec![0;8];
     let mut number_of_lines = 0;
     let mut curr_line_buffer = Vec::new();
@@ -18,10 +23,14 @@ async fn main() -> tokio::io::Result<()>{
 
         for &b in &chunk[..len] {
             if b == b'\n' {
-                let curr_string = String::from_utf8_lossy(&curr_line_buffer);
-                println!("{:?}", curr_string);
+                let curr_string = String::from_utf8_lossy(&curr_line_buffer).to_string();
+                // println!("{:?}", curr_string);
+                if let Err(_) = tx.send(curr_string).await {
+                    println!("Receiver dropped!");
+                    return Ok(());
+                }
                 number_of_lines += 1;
-                curr_line_buffer = Vec::new();
+                curr_line_buffer.clear();
             } else {
                 curr_line_buffer.push(b);
             }
@@ -36,5 +45,25 @@ async fn main() -> tokio::io::Result<()>{
     }
 
     println!("The file has {} number of lines", number_of_lines);
+    
+    Ok(())
+} 
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut file = File::open("messages.txt").await?;
+    // print_type(&file);
+
+    let (tx, mut rx): (mpsc::Sender<String>, mpsc::Receiver<String>) = mpsc::channel(100);
+
+    // print_type(&rx);
+    tokio::spawn(async move {
+        get_lines_channel(file, tx).await;
+    });
+
+    while let Some(line) = rx.recv().await {
+        println!("current line : {}", line);
+    }
+    
     Ok(())
 }
